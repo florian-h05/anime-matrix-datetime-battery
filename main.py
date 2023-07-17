0b0b45239fa4a29d91ab226cbb94a8847a26db38
Florian Hotze <florianh_dev@icloud.com>
2023-07-17 16:41:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import psutil
 import time
 from datetime import datetime
 
-# Do not use ROG button, as access to the X server is not possible from systemd services.
-# from pynput import keyboard
-
 # VARS
 
 fontname   = "Hack-Regular.ttf" # Font (must be in same directory)
@@ -61,28 +58,9 @@ def cleanup(signal, frame):
 	system("asusctl anime -e false") # Turn off matrix on sigint
 	sys.exit(0)
 
-def on_press(key):
-	try: # Get keycode
-		vk = key.vk
-	except AttributeError:
-		vk = key.value.vk
-	if vk == keybind:
-		global active
-		active = not active # Invert active state
-		if active:
-			system("asusctl anime -e true") # Enable display
-			main() # Update display
-		else:
-			system("asusctl anime -e false") # Disable display
-
 if __name__ == "__main__":
 	system("asusctl anime -e true") # Enable display
 	signal.signal(signal.SIGINT, cleanup) # Register SIGINT handler
-	# Do not use ROG button, as access to the X server is not possible from systemd services.
-	#listener = keyboard.Listener( # Register keyboard listener
-	#	on_press=on_press
-	#)
-	#listener.start()
 	while True:
 		if active :
 			main()
